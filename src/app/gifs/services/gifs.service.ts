41de97fdff271497b78a4c68d62f8d7e11dd9e59
albertost5 <sancheztorresalberto@gmail.com>
2023-12-22 12:53:42 +0100

    Add validation to gifs service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,20 @@ export class GifsService {
 
   searchTag(tag: string): void {
     // TODO: make the API call
-    this._tagsHistory.unshift(tag);
+    if (tag.trim().length === 0) return;
+
+    const lowerCaseTag = tag.toLowerCase();
+
+    this.validateAndAddTag(lowerCaseTag);
   }
+
+  private validateAndAddTag(tag: string): void {
+    if ( this._tagsHistory.includes(tag) ) {
+      this._tagsHistory = this._tagsHistory.filter( t => t !== tag);
+    }
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.slice(0,10);
+  }
+
   constructor() { }
 }
